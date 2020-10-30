5d18b6349920f561e4e42ec93405a062fe3436c3
Amanda Carpentieri <mandac@Amandas-MacBook-Air.local>
2020-10-30 11:27:22 -0700

    added toggle between x and o

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let cells = document.querySelectorAll('.row > div');
+clickCount = 0
 
 for (let i = 0; i < cells.length; i++){
     cells[i].addEventListener("click", cellClicked);
@@ -7,3 +8,13 @@ for (let i = 0; i < cells.length; i++){
 function cellClicked () {
     event.target.textContent = "X"
 }
+
+function cellClicked( event ) {
+    if( clickCount % 2 === 0 && event.target.textContent !== "O" ) {
+         event.target.textContent = "X";
+         ++clickCount;
+    } else if( clickCount % 2 === 1 && event.target.textContent !== "X" )
+    {
+         event.target.textContent = "O";
+         ++clickCount;
+    }};
